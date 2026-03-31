8c8fce8a5bac30a11ba10455122a819a389f8602
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-03-31 14:20:43 +0300

    Imported required libraries and added states

---
+++ admin-panel/src/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import api from "../api/api";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const nav = useNavigate();
+
+  
+}
