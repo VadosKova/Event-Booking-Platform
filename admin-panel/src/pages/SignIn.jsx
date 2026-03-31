4ca04d7c47d3d56f0b8ce12ee8f12d84abbcc40e
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-03-31 14:23:10 +0300

    Created function for login and interface

--- admin-panel/src/pages/SignIn.jsx
+++ admin-panel/src/pages/SignIn.jsx
@@ -7,5 +7,24 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const nav = useNavigate();
 
-  
+  const login = async () => {
+    try {
+      const res = await api.post("/auth/login", { email, password });
+
+      localStorage.setItem("token", res.data.token);
+
+      nav("/events");
+    } catch (err) {
+      alert("Login error");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Admin Login</h2>
+      <input placeholder="email" onChange={e => setEmail(e.target.value)} />
+      <input placeholder="password" type="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={login}>Login</button>
+    </div>
+  );
 }
