829bb3e40e020c34e534e8ab234e0958d761d751
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-03-31 12:44:24 +0300

    Created POST method for auth

--- user-service/routes/auth.js
+++ user-service/routes/auth.js
@@ -14,4 +14,21 @@ router.post("/register", async (req, res) => {
   res.json(user);
 });
 
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) return res.status(400).send("User not found");
+
+  const valid = await bcrypt.compare(req.body.password, user.password);
+
+  if (!valid) return res.status(400).send("Wrong password");
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET
+  );
+
+  res.json({ token });
+});
+
 module.exports = router;
