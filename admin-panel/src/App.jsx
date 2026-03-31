0660b2b45772b4076f0037f07589e29451709854
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-03-31 14:42:49 +0300

    Updated main file for loading pages

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,12 +1,25 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/Login";
+import Events from "./pages/Events";
+import Users from "./pages/Users";
+import Bookings from "./pages/Bookings";
 import './App.css'
 
-function App() {
-
+export default function App() {
   return (
-    <>
-      
-    </>
-  )
-}
+    <BrowserRouter>
+      <nav>
+        <Link to="/events">Events</Link> | 
+        <Link to="/users">Users</Link> | 
+        <Link to="/bookings">Bookings</Link>
+      </nav>
 
-export default App
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/bookings" element={<Bookings />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
