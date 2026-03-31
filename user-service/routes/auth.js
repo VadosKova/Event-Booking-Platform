9a67df7cfb66803928b487a794ed2b59655d2671
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-03-31 12:43:10 +0300

    Created User model and POST method for register

---
+++ user-service/routes/auth.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+router.post("/register", async (req, res) => {
+  const hashed = await bcrypt.hash(req.body.password, 10);
+
+  const user = await User.create({
+    ...req.body,
+    password: hashed
+  });
+
+  res.json(user);
+});
+
+module.exports = router;
