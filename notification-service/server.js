baa812d7b3f86f889ea4ac8cf3f2be11e4c7c2c9
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-03-31 15:49:21 +0300

    Fixed bugs

--- notification-service/server.js
+++ notification-service/server.js
@@ -1,7 +1,18 @@
 const amqp = require("amqplib");
 
+async function connectRabbit() {
+  try {
+    const conn = await amqp.connect("amqp://rabbitmq");
+    return conn;
+  } catch (err) {
+    console.log("Waiting for RabbitMQ...");
+    await new Promise(res => setTimeout(res, 3000));
+    return connectRabbit();
+  }
+}
+
 async function start() {
-  const conn = await amqp.connect("amqp://rabbitmq");
+  const conn = await connectRabbit();
   const ch = await conn.createChannel();
 
   await ch.assertQueue("booking");
